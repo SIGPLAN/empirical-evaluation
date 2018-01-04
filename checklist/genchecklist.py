8e8db52905d301333b8a51b35a1052525cefce14
Steve Blackburn <steve.blackburn@anu.edu.au>
2018-01-05 00:01:06 +1100

    First very rough cut at scripted checklist generation

---
+++ checklist/genchecklist.py
@@ -0,0 +1,71 @@
+#!/usr/bin/env python3
+import sys
+import getopt
+import yaml
+import string
+
+def usage(errno):
+    print ('usage: ',sys.argv[0], '-c config.yml')
+    sys.exit(errno)
+
+
+def genlatex(title,credits,date,halfrows):
+    print("\documentclass{article}\n\\usepackage[left=1cm, right=1cm, top=1cm, bottom=1cm]{geometry}\n\\usepackage{multirow}\n\\usepackage[svgnames]{xcolor}\n\\usepackage{colortbl}\n\\usepackage{array}\n\\usepackage{svg}\n\\usepackage{graphicx}\n\\usepackage[scaled]{helvet} % see www.ctan.org/get/macros/latex/required/psnfss/psnfss2e.pdf\n\\begin{document}\n\pagenumbering{gobble}\n\sffamily\n\\begin{center}\\textbf{\LARGE "+title+" \large (alpha version)}\end{center}\n\scriptsize\n\\begin{centering}\\begin{tabular}{ccccccc}")
+
+    
+    rows = int((len(halfrows)+1)/2)
+    for r in range(0, rows):
+        print(halfrows[r],"&&",halfrows[r+rows],"\\\\")
+
+    print("\end{tabular}\n\end{centering}\n\\vfill\n\hfill \scriptsize "+date+". "+credits+"\n\end{document}")
+
+
+def processgroup(group, halfrows):
+    color=group['color']
+    keyword=group['keyword']
+    name=group['name'].replace(keyword, "\\textbf{"+keyword+"}")
+    items=len(group['items'])
+    for i in range(0, items):
+        item=group['items'][i]
+        ltx="\cellcolor{"+color+"}"
+        if (i == items -1):
+            ltx+= "\multirow{-"+str(items)+"}{*}{\smash{\\rotatebox[origin=tl]{90}{\\normalsize "+name+"}}}"
+        ltx+="& \cellcolor{"+color+"}\includegraphics[width=2.1cm]{"+item['figure']+"} &\cellcolor{"+color+"}\\begin{minipage}[b][2.1cm][t]{.3\\textwidth}\\vspace{1em}\\textbf{\small "+item['name']+"}\\\\"+item['desc']+"\end{minipage}"
+        halfrows.append(ltx)
+    
+def gen(config):
+    stream = open(config, "r")
+    ol = yaml.load_all(stream)
+    halfrows = []
+    for i in ol:
+        groups=i['groups']
+        for g in groups:
+            processgroup(g, halfrows)
+    stream.close()
+    genlatex(i['title'],i['credits'],i['date'],halfrows)
+
+def main(argv):
+    config = None
+  
+    try:
+        opts, args = getopt.getopt(argv,"c:",["config="])
+    except getopt.GetoptError:
+        usage(2)
+    for opt, arg in opts:
+        if opt == '-h':
+            usage(0)
+        elif opt in ("-c", "--config"):
+            config = arg
+
+
+    if config == None:
+        print ('You must specify a config file')
+        usage(2)
+    else:
+        gen(config)
+        
+    exit(0)
+
+
+if __name__ == "__main__":
+   main(sys.argv[1:])
