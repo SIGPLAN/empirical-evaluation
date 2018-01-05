df82b1533b008161a991355e2bad181803a3cc28
Steve Blackburn <steve.blackburn@anu.edu.au>
2018-01-05 16:24:05 +1100

    Cleanup and some minimal documentation

---
+++ checklist/README.md
@@ -0,0 +1,15 @@
+## Emperical Evaluation Checklist
+
+An up-to-date version of the SIGPLAN emperical evaluation checklist should always be available [here](checklist.pdf).
+
+### Feedback
+
+We welcome feedback and suggestions for the checklist.
+
+### Reading and maintaining 
+
+The checklist is maintained as a plain-text [yaml](http://yaml.org) file [here](checklist.yaml), and a series of figures which are mained as pdfs [here](figs/), but are sourced from [this](https://docs.google.com/presentation/d/1sdSNQTRRvSTgCUO7S4_w2oeNVIbX_SqsJvX_A-XFJ5M/edit?usp=sharing) Google Presentation.
+
+### Generating a pdf
+
+A pdf can be generated from the yaml using [this script](genchecklist.py).
--- checklist/genchecklist.py
+++ checklist/genchecklist.py
@@ -1,24 +1,102 @@
 #!/usr/bin/env python3
+#
+# This script will take a yaml description of a checklist and
+# produce a latex file from which a pdf can be generated.
+#
+# The latex is encoded as template strings near the top of
+# this string.
+#
+# Example usage:
+#
+# % genchecklist.py -c config.yml -l checklist.tex
+# % pdflatex checklist.tex
+#
+#
 import sys
 import getopt
 import yaml
 import string
+from string import Template
 
 def usage(errno):
-    print ('usage: ',sys.argv[0], '-c config.yml')
+    print ('usage: ',sys.argv[0], '-c config.yml -l checklist.tex')
     sys.exit(errno)
 
+# latex prolog
+ltxprolog = r"""
+\documentclass{article}
+\usepackage[left=1cm, right=1cm, top=1cm, bottom=1cm]{geometry}
+\usepackage{multirow}
+\usepackage[svgnames]{xcolor}
+\usepackage{colortbl}
+\usepackage{array}
+\usepackage{svg}
+\usepackage{graphicx}
+\usepackage{hhline}
+\usepackage[scaled]{helvet} % see www.ctan.org/get/macros/latex/required/psnfss/psnfss2e.pdf
+\setlength{\arrayrulewidth}{.15em}
 
-def genlatex(title,version,credits,date,halfrows,halfrulecolor):
-    print("\documentclass{article}\n\\usepackage[left=1cm, right=1cm, top=1cm, bottom=1cm]{geometry}\n\\usepackage{multirow}\n\\usepackage[svgnames]{xcolor}\n\\usepackage{colortbl}\n\\usepackage{array}\n\\usepackage{svg}\n\\usepackage{graphicx}\n\\usepackage[scaled]{helvet} % see www.ctan.org/get/macros/latex/required/psnfss/psnfss2e.pdf\n\\usepackage{hhline}\n\\usepackage{amssymb}\n\setlength{\\arrayrulewidth}{.15em}\n\\begin{document}\n\pagenumbering{gobble}\n\sffamily\n\\begin{center}\\textbf{\huge "+title+"} {\\normalsize ("+version+")}\end{center}\n\scriptsize\n\\begin{centering}\\begin{tabular}{ccccccc}")
+\begin{document}
+\pagenumbering{gobble}
+\sffamily
+
+\begin{center}
+  \textbf{\huge $title {\normalsize ($version)}}
+\end{center}
+
+\scriptsize
+\begin{centering}
+  \begin{tabular}{ccccccc}
+"""
+
+# latex row
+ltxrow =  r"""
+  $halfrowl && $halfrowr \\ \hhline{>{\arrayrulecolor{$colorl}}->{\arrayrulecolor{white}}-->{\arrayrulecolor{white}}->{\arrayrulecolor{$colorr}}->{\arrayrulecolor{white}}--}
+"""
+
+# latex halfrow
+ltxhalfrow = r"""
+& \cellcolor{$color} \includegraphics[width=1.8cm]{$figure} &\cellcolor{$color}
+\begin{minipage}[b][2.1cm][t]{.305\textwidth}\vspace{1em}
+\textbf{\small $name}\\
+$description
+\end{minipage}
+"""
+
+# latex group name and checkbox in rotated box
+ltxgroupname = r"""
+\multirow{-$rows}{*}{\smash{\rotatebox[origin=tl]{90}{\hspace{-2ex}\normalsize {\setlength{\fboxrule}{0.125pt}\raisebox{.5ex}{\fcolorbox{black}{white}{\rule{0pt}{.5ex}~}}}~~~$name}}}
+"""
+
+# latex epilog
+ltxepilog = r"""
+  \end{tabular}
+\end{centering}
+\vfill
+\hfill \scriptsize $date. $credits
+\end{document}
+"""
+
+#
+# Generate the latex for the whole document
+#
+def genlatex(ltxfile,title,version,credits,date,halfrows,halfrulecolor):
+    with open(ltxfile,'w') as f:
+        t=Template(ltxprolog)
+        f.write(t.substitute({ 'title': title, 'version': version}))
+        
+        rows = int((len(halfrows)+1)/2)
+        t=Template(ltxrow)
+        for r in range(0, rows):
+            f.write(t.substitute({ 'halfrowl' : halfrows[r], 'halfrowr' : halfrows[r+rows], 'colorl' : halfrulecolor[r], 'colorr' : halfrulecolor[r+rows] }))
+            
+        t=Template(ltxepilog)
+        f.write(t.substitute({ 'date': date, 'credits': credits}))
+    f.closed
     
-    rows = int((len(halfrows)+1)/2)
-    for r in range(0, rows):
-        print(halfrows[r],"&&",halfrows[r+rows],"\\\\ \\hhline{>{\\arrayrulecolor{"+halfrulecolor[r]+"}}->{\\arrayrulecolor{white}}-->{\\arrayrulecolor{white}}->{\\arrayrulecolor{"+halfrulecolor[r+rows]+"}}->{\\arrayrulecolor{white}}--}")
-
-    print("\end{tabular}\n\end{centering}\n\\vfill\n\hfill \scriptsize "+date+". "+credits+"\n\end{document}")
-
-
+#
+# Process a complete checklist group, creating half-rows and rule colors
+#
 def processgroup(group, halfrows, halfrulecolor):
     color=group['color']
     keyword=group['keyword']
@@ -28,17 +106,20 @@ def processgroup(group, halfrows, halfrulecolor):
         item=group['items'][i]
         ltx="\cellcolor{"+color+"}"
         if (i == items -1):
- #           ltx+= "\multirow{-"+str(items)+"}{*}{\smash{\\rotatebox[origin=tl]{90}{\\normalsize "+name+"~~~{\large $\square$}}}}"
-            ltx+= "\multirow{-"+str(items)+"}{*}{\smash{\\rotatebox[origin=tl]{90}{\hspace{-2ex}\\normalsize {\setlength{\\fboxrule}{0.125pt}\\raisebox{.5ex}{\\fcolorbox{black}{white}{\\rule{0pt}{.5ex}~}}}~~~"+name+"}}}"
-#            ltx+= "\multirow{-"+str(items)+"}{*}{\smash{\\rotatebox[origin=tl]{90}{\\normalsize "+name+"}}}"
+            t=Template(ltxgroupname)
+            ltx+= t.substitute({ 'rows' : str(items), 'name' : name})
             halfrulecolor.append('white')
         else:
             halfrulecolor.append(color)
-        ltx+="& \cellcolor{"+color+"}\includegraphics[width=2.1cm]{"+item['figure']+"} &\cellcolor{"+color+"}\\begin{minipage}[b][2.1cm][t]{.3\\textwidth}\\vspace{1em}\\textbf{\small "+item['name']+"}\\\\"+item['desc']+"\end{minipage}"
+
+        t=Template(ltxhalfrow)
+        ltx+=t.substitute({ 'color': color, 'figure': item['figure'], 'name': item['name'], 'description': item['desc'] })
         halfrows.append(ltx)
         
-   
-def gen(config):
+#
+# parse the yaml and generate the latex
+#
+def parseandgen(config, ltxfile):
     stream = open(config, "r")
     ol = yaml.load_all(stream)
     halfrows = []
@@ -48,27 +129,34 @@ def gen(config):
         for g in groups:
             processgroup(g, halfrows, halfrulecolor)
     stream.close()
-    genlatex(i['title'],i['version'],i['credits'],i['date'],halfrows,halfrulecolor)
+    genlatex(ltxfile,i['title'],i['version'],i['credits'],i['date'],halfrows,halfrulecolor)
+
 
 def main(argv):
     config = None
+    ltxfile = None
   
     try:
-        opts, args = getopt.getopt(argv,"c:",["config="])
+        opts, args = getopt.getopt(argv,"c:l:",["config=","latex="])
     except getopt.GetoptError:
         usage(2)
+
     for opt, arg in opts:
         if opt == '-h':
             usage(0)
         elif opt in ("-c", "--config"):
             config = arg
-
+        elif opt in ("-l", "--latex"):
+            ltxfile = arg
 
     if config == None:
-        print ('You must specify a config file')
+        print ('You must specify a yaml input config file')
+        usage(2)
+    elif ltxfile == None:
+        print ('You must specify an output latex file')
         usage(2)
     else:
-        gen(config)
+        parseandgen(config, ltxfile)
         
     exit(0)
 
